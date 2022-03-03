134a027005b18caf915b4f1ff0db93e9a9a98d7b
ayush1852017 <46336517+ayush1852017@users.noreply.github.com>
2022-03-04 02:18:49 +0530

    hft

---
+++ sprint 1/day 1/main.js
@@ -0,0 +1,8 @@
+const add = require("./add");
+console.log("add", add(1, 2));
+const substract = require("./subtract");
+console.log("substract:", substract(2, 3));
+const multiplication = require("./multiply");
+console.log("multiplication:", multiplication(10, 2));
+const division = require("./divide");
+console.log("division:", division(4, 2));
